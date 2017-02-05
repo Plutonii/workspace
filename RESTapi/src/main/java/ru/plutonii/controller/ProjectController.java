1dc621be2569df526a7123305d12fe3a412715eb
Roman Kreidzich <roman1.kreidich@yandex.ru>
2017-02-05 21:47:30 +0300

    add project controller

---
+++ RESTapi/src/main/java/ru/plutonii/controller/ProjectController.java
@@ -0,0 +1,45 @@
+package ru.plutonii.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.plutonii.model.Project;
+import ru.plutonii.service.ProjectService;
+
+import java.util.List;
+
+/**
+ * Created by plutonii on 05.02.17.
+ */
+@RestController
+@RequestMapping("/api")
+public class ProjectController {
+
+    private ProjectService projectService;
+
+    @Autowired
+    ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping(path = "/project/{id}")
+    @ResponseBody
+    Project getProjectById(@PathVariable(name = "id") int id) {
+        return projectService.findById(id);
+    }
+
+    @GetMapping(path = "/{id}/project")
+    @ResponseBody
+    List<Project> getProjectsByUserId(@PathVariable(name = "id") int id){
+        return projectService.findByUserId(id);
+    }
+
+    @PostMapping("/project")
+    Project addProject(@RequestBody Project project){
+        return projectService.insert(project);
+    }
+
+    @DeleteMapping("/{id}/project")
+    void deleteProject(@PathVariable(name = "id") int id){
+        projectService.delete(id);
+    }
+}
