ca4c7cadfbea1a3e337b1d56963ad671586f538e
Roman Kreidzich <roman1.kreidich@yandex.ru>
2017-02-07 17:51:38 +0300

    rename

---
+++ RESTapi/src/main/java/ru/plutonii/service/AuthenticationService.java
@@ -0,0 +1,9 @@
+package ru.plutonii.service;
+
+
+/**
+ * Created by plutonii on 04.02.17.
+ */
+public interface AuthenticationService {
+    boolean authentication(String token);
+}
---
+++ RESTapi/src/main/java/ru/plutonii/service/AuthenticationServiceImpl.java
@@ -0,0 +1,36 @@
+package ru.plutonii.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.plutonii.dao.TokenDAO;
+import ru.plutonii.model.Token;
+
+
+/**
+ * Created by plutonii on 05.02.17.
+ */
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private TokenDAO tokenDAO;
+
+    @Autowired
+    AuthenticationServiceImpl(TokenDAO tokenDAO){
+        this.tokenDAO = tokenDAO;
+    }
+
+    public boolean authentication(String token) {
+        int indexSeparator = token.indexOf('|');
+        if (indexSeparator == -1) return false;
+        try {
+            int id = Integer.parseInt(token.substring(0, indexSeparator));
+            token = token.substring(indexSeparator);
+            Token realToken = tokenDAO.findByUserId(id);
+            if (realToken == null) return false;
+            if (!realToken.getToken().equals(token)) return false;
+        } catch (NumberFormatException NFE) {
+            return false;
+        }
+        return true;
+    }
+}
